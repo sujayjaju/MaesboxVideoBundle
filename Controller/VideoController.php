5f635e121c23e40113b2eb9a99f6f58275f3a4cf
maerianne <troudier@TROUDIER.f6.fr>
2013-09-17 18:35:15 +0200

    adding controller

---
+++ Controller/VideoController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Maesbox\VideoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+
+class VideoController extends Controller
+{
+    
+    
+}
